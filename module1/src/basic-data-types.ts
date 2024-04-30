6b42726cfadc051cfe59de47cfe32c5540ea357a
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-04-30 23:23:52 +0600

    data types

---
+++ module1/src/basic-data-types.ts
@@ -0,0 +1,21 @@
+// Basic data types of typescript
+
+// string
+let firstName = "Nahidul Islam" // if we don't declare the data types and typescript compiler assumes the data types internally, then it is called implicit data types.
+let secondName: string = "Fahim" // if we explicitly declare the data types, then it is called explicit data types.
+
+// number
+let rollNumber: number = 122;
+
+// boolean
+let isSuperHero: boolean = false;
+
+// undefined
+let x: undefined = undefined;
+
+// null
+let y: null = null
+
+let z: any
+
+// we should not declare 'any' data type. By this, we are actually ignoring the power of typescript.
