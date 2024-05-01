555c5c2b8b2e27cf0679e327b0a50c641bff303e
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-05-01 23:32:04 +0600

    ternary, optional

---
+++ module1/src/ternary-optional-nullish.ts
@@ -0,0 +1,28 @@
+{
+    // ternary operator
+
+    const age: number = 16;
+    const isAdult = age >= 18 ? "adult" : "not adult";
+
+
+
+    /* ============================== */
+
+
+    // nullish coalescing operator
+    // this operator will be used if only we have to make any decision based on 'null' and 'undefined'
+    const isAuthenticated = null;
+    const result1 = isAuthenticated ?? "guest"; // this operator only works based on null of undefined
+
+
+    /* ============================== */
+
+
+
+
+
+
+
+
+
+}
