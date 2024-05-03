e19b1f19f9697db84aadbad87a3067143cd4a352
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-05-03 18:01:31 +0600

    generic with interface

---
+++ module2/src/generic-with-interface.ts
@@ -0,0 +1,67 @@
+{
+    // generic interface type
+
+    interface Developer<T> {
+        name: string,
+        computer: {
+            brand: string,
+            model: string,
+            releaseYear: number
+        },
+        smartWatch: T
+    }
+
+    // type alias for smartwatch
+    type SmartWatchType = {
+        brand: string,
+        model: string,
+        display: string,
+        heartCheck?: boolean,
+        staminaCheck?: boolean
+    }
+
+
+
+    // object 1
+    const developer1: Developer<SmartWatchType> = {
+        name: "Nahid",
+        computer: {
+            brand: "HP",
+            model: "Hj587",
+            releaseYear: 2018
+        },
+        smartWatch: {
+            brand: "Osaka",
+            model: "41fdj",
+            display: "LED"
+        }
+    }
+
+    // object 2
+    const developer2: Developer<SmartWatchType> = {
+        name: "Nahid",
+        computer: {
+            brand: "HP",
+            model: "Hj587",
+            releaseYear: 2018
+        },
+        smartWatch: {
+            brand: "Osaka",
+            model: "41fdj",
+            display: "LED",
+            heartCheck: true,
+            staminaCheck: true
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
