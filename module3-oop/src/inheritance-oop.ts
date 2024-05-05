9cb393169d8f05091ad19291622ae331d06b0a82
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-05-05 23:24:13 +0600

    inheritance in OOP

---
+++ module3-oop/src/inheritance-oop.ts
@@ -0,0 +1,70 @@
+{
+    // Inheritance in OOP
+
+    // Here the class of Student and Teacher is the same. So, we can make a Parent class using this same class.
+
+    // Parent class
+    class Parent {
+        name: string;
+        goodIn: string;
+        address: string;
+
+        constructor(name: string, goodIn: string, address: string) {
+            this.name = name;
+            this.goodIn = goodIn;
+            this.address = address;
+        }
+
+        intro() {
+            console.log(`Our very own, ${this.name} is good in ${this.goodIn}.`)
+        }
+    }
+
+
+    // class - student
+    class Student extends Parent {
+        constructor(name: string, goodIn: string, address: string) {
+            super(name, goodIn, address); // as we will send value from child to parent, so need a way to send that value. Which is 'super()'
+        }
+    }
+
+    const student1 = new Student("Nahidul Islam", "Programming", "Chattogram");
+    student1.intro();
+
+
+    // another class - teacher
+    class Teacher extends Parent {
+        designation: string;
+
+        constructor(name: string, goodIn: string, address: string, designation: string) {
+            super(name, goodIn, address)
+            this.designation = designation;
+        }
+
+    }
+
+    const teacher1 = new Teacher("Mr. Nurul Absar", "math", "Chattogram", "Head Teacher");
+    teacher1.intro();
+
+
+
+
+
+
+
+
+
+
+
+    /* ==================================== */
+
+
+
+
+
+
+
+
+
+
+}
